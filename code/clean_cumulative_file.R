41003efbf86eafa22a8c65c19035e4599f557664
DamonCharlesRoberts <damon.roberts-1@colorado.edu>
2022-03-01 10:55:57 -0700

    early cleaning

---
+++ code/clean_cumulative_file.R
@@ -0,0 +1,59 @@
+# Title: Constructing macropartisanship ----
+
+# Notes: ----
+    #* Description: script for loading anes cumulative time series and then converting it into macropartisan and macro-vote choice series ----
+    #* Updated: 2022 - 03 - 01 ----
+    #* Updated by: dcr
+
+# Setup: ----
+    #* Modularly load functions from packages ----
+#install.packages('box')#package used for modularly loading functions
+library(anesr)
+box::use(
+    dplyr = dplyr[rename, select, mutate, case_when, group_by, summarize]
+)
+    #* Load ANES time series cumulative file ----
+data(timeseries_cum)
+timeseries_cum = timeseries_cum |>
+    rename(year = VCF0004)
+# Constructing series from cumulative file ----
+    #* vote choice ----
+voteDem = timeseries_cum |>
+    select(VCF0707, year) |>
+    mutate(VCF0707 = case_when(VCF0707 == 1 ~ 1,
+                                VCF0707 == 2 ~ 0)) |>
+    group_by(year) |>
+    summarize(voteDem = mean(VCF0707, na.rm = TRUE))
+    #* pid ----
+pid = timeseries_cum |>
+    select(VCF0301, year) |>
+    group_by(year) |>
+    summarize(pid = mean(VCF0301, na.rm = TRUE))
+    #* ideology ----
+ideo = timeseries_cum |>
+    select(VCF0803, year)|>
+    group_by(year) |>
+    summarize(ideo = mean(VCF0803, na.rm = TRUE))
+    #* gender ----
+female = timeseries_cum |>
+    select(VCF0104, year) |>
+    mutate(VCF0104 = case_when(VCF0104 == 1 ~ 0,
+                                VCF0104 == 2 ~ 1)) |>
+    group_by(year) |>
+    summarize(gender = mean(VCF0104, na.rm = TRUE))
+    #* race ----
+white = timeseries_cum |>
+    select(VCF0105b, year) |>
+    mutate(VCF0105b = ifelse(VCF0105b == 1, 1, 0)) |>
+    group_by(year) |>
+    summarize(race = mean(VCF0105b, na.rm = TRUE))
+black = timeseries_cum |>
+    select(VCF0105b, year) |>
+    mutate(VCF0105b = ifelse(VCF0105b == 2, 1, 0)) |>
+    group_by(year) |>
+    summarize(race = mean(VCF0105b, na.rm = TRUE))
+hispanic = timeseries_cum |>
+    select(VCF0105b, year) |>
+    mutate(VCF0105b = ifelse(VCF0105b == 3, 1, 0)) |>
+    group_by(year) |>
+    summarize(race = mean(VCF0105b, na.rm = TRUE))
---
+++ code/cleaning_2018_pilot.R
@@ -0,0 +1,60 @@
+# Title: Cleaning from 2018 pilot study ----
+
+# Notes: ----
+    #* Description: script for loading 2018 ANES panel and then converting it into macropartisan and macro-vote choice series ----
+    #* Updated: 2022 - 03 - 01 ----
+    #* Updated by: dcr
+
+# Setup: ----
+    #* Modularly load functions from packages ----
+#install.packages('box')#package used for modularly loading functions
+library(anesr)
+box::use(
+    dplyr = dplyr[rename, select, mutate, case_when, group_by, summarize]
+)
+    #* Load ANES 2018 pilot file ----
+data(pilot_2018)
+pilot_2018 = pilot_2018
+# Constructing series from 2018 pilot file ----
+    #* Vote choice ----
+voteDem = pilot_2018 |>
+    select(house18p, year) |>
+    mutate(house18p = case_when(house18p == 1 ~ 1,
+                                house18p == 2 ~ 0)) |>
+    summarize(voteDem = mean(house18p, na.rm = TRUE))
+    #* pid ----
+pid = pilot_2018 |>
+    select(pid1r, pid1d, pidstr, pidlean, year) |>
+    mutate(pid = case_when(pid1d == 3 | pid1d == 4 | pid1r == 3 | pid1d == 4 & pidlean == 3 ~ 0, #report indpendent or something else and report not close to either; true independent
+                          pid1d == 3 | pid1d == 4 | pid1r == 3 | pid1d == 4 & pidlean == 2 ~ 1, #report independent or something else and report closer to democrats; lean democrat
+                          pid1d == 3 | pid1d == 4 | pid1r == 3 | pid1d == 4 & pidlean == 1 ~ -1, #report independent or something else and report closer to republicans; lean republican
+                          pid1d == 1 | pid1r == 1 & pidstr == 2 ~ 1, #report democrat and report not very strong
+                          pid1d == 1 | pid1r == 1 & pidstr == 1 ~ 2, #report democrat and report strong
+                          pid1d == 2 | pid1r == 2 & pidstr == 2 ~ -1, #report republican and report not very strong
+                          pid1d == 2 | pid1r == 2 & pidstr == 1 ~ -2 #report republican and report strong
+                          )) |>
+    summarize(pid = mean(pid, na.rm = TRUE))
+    #* ideology ----
+ideo = pilot_2018 |>
+    select(lcself) |>
+    mutate(lcself = ifelse(lcself < 0, NA, lcself)) |>
+    summarize(ideo = mean(lcself, na.rm = TRUE))
+    #* gender ----
+female = pilot_2018 |>
+    select(gender) |>
+    mutate(female = case_when(gender == 1 ~ 0,
+                            gender == 2 ~ 1)) |>
+    summarize(ideo = mean(female, na.rm = TRUE))
+    #* race ----
+white = pilot_2018 |>
+    select(race) |>
+    mutate(white = ifelse(race == 1, 1, 0)) |>
+    summarize(white = mean(white, na.rm = TRUE))
+black = pilot_2018 |>
+    select(race) |>
+    mutate(black = ifelse(race == 2, 1, 0)) |>
+    summarize(black = mean(black, na.rm = TRUE))
+hispanic = pilot_2018 |>
+    select(race) |>
+    mutate(hispanic = ifelse(race == 3, 1, 0)) |>
+    summarize(hispanic = mean(hispanic, na.rm = TRUE))
