9f0c65b020046289aa64e1260d116f86d84ef969
DamonCharlesRoberts <damon.roberts-1@colorado.edu>
2022-03-01 21:00:03 -0700

    combining data

---
+++ code/combined_data.R
@@ -0,0 +1,15 @@
+# Title: Combining the data ----
+
+# Notes: ----
+    #* Description: script for sourcing the cleaning scripts, merging the data, then saving it in a time series format ----
+    #* Updated: 2022 - 03 - 01 ----
+    #* Updated by: dcr
+
+# Setup: ----
+    #* Modularly load functions from packages ----
+#install.packages('box')#package used for modularly loading functions
+    #* Sourcing cleaning scripts ----
+source('code/clean_cumulative_file.R')
+source('code/cleaning_2006_pilot.R')
+source('code/cleaning_2018_pilot.R')
+
